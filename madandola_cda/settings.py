d09e669baf61b7f6e383318db6f28cdb7b6f8e29
Salami Ibrahim <hizeetech@gmail.com>
2025-06-29 13:55:11 +0100

    Fix 400 error by updating ALLOWED_HOSTS

--- madandola_cda/settings.py
+++ madandola_cda/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['madandolaestate.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
