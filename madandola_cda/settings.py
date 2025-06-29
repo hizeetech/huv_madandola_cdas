fabcc7270fa5dff2ecdc02845c84023ed6e3add5
Salami Ibrahim <hizeetech@gmail.com>
2025-06-29 14:00:34 +0100

    Fix 400 error by adding Render domain to ALLOWED_HOSTS

--- madandola_cda/settings.py
+++ madandola_cda/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['madandolaestate.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
 
 
